39720dcbe337c6982181b167153aacbc9cb9ab14
Adewale Akanni <waleakanni97@gmail.com>
2023-05-08 19:47:52 +0100

    updating layouts.content

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -23,5 +23,10 @@
     @include('partials.footer')
 
 </body>
+<script src="{{asset('js/jquery-3.3.1.js')}}"></script>
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
 </html>
